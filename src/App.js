4b136287a2e9739792f8fed4bfd01a3ebe75bde1
Keona Kirby <keonakirby@gmail.com>
2021-05-18 23:24:31 -0500

    added route paths in app.js

--- src/App.js
+++ src/App.js
@@ -1,6 +1,23 @@
 import React from "react";
-import { BrowseRouter as Router, Route } from "react-router-dom";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import Hero from "./components/pages/Hero";
+import About from "./components/pages/About";
+import Project from "./components/pages/Project";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
 
-function App() {}
+function App() {
+  return (
+    <Router>
+      <div>
+        <Header />
+        <Route path="/" component={Hero} />
+        <Route exact path="/about" component={About} />
+        <Route exact path="/Project" component={Project} />
+        <Footer />
+      </div>
+    </Router>
+  );
+}
 
 export default App;
