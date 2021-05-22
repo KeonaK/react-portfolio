65377e4cbe9f2567b84cc85b1d74546e67e9dba0
Keona Kirby <keonakirby@gmail.com>
2021-05-22 11:29:31 -0500

    added google books to work section

--- src/workFolder/Placeholder.js
+++ src/workFolder/Placeholder.js
@@ -1,21 +1,37 @@
 import React from "react";
-import X from "../images/250x250.svg";
+import Google from "../images/250x250.svg";
 import "../App.js";
 
 function Placeholder() {
   return (
     <div>
       <div>
-        <div class="project-img">
-          <img src={X} alt="project" />
-        </div>
-        <h3>Project 5</h3>
-        <p>project description</p>
+        <img src={Google} alt="project" />
+      </div>
+      <div>
+        <h3>Google-Books-Search</h3>
+        <p>
+          This app allows the user to search for books using the Google Books
+          Api. The user is able to view and save their favorite books!
+        </p>
         <button class="project_button btn" type="button">
-          View Deployment{" "}
+          {" "}
+          <a
+            target="_blank"
+            href="https://obscure-scrubland-75542.herokuapp.com/"
+          >
+            View Deployment
+          </a>
         </button>
         <button class="project_button btn" type="button">
-          View Repository
+          {" "}
+          <a
+            target="_blank"
+            rel="noreferrer"
+            href="https://github.com/KeonaK//google-books-search"
+          >
+            View Repository
+          </a>
         </button>
       </div>
     </div>
